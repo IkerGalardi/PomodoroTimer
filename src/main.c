f7cad869a4cd5fb34f715dca87c56e2ba8b7206b
IkerGalardi <contacto.ikergalardi@gmail.com>
2026-07-02 11:20:37 +0200

    app: add configuration defines
    Configuring through the GUI is planned as a feature, but for now its good to be
    able to configure it at least at compile time.

--- src/main.c
+++ src/main.c
@@ -6,21 +6,30 @@
 #include <raylib.h>
 #include <raygui.h>
 
-#define SCREEN_WIDTH 800
-#define SCREEN_HEIGHT 800
-#define TIMER_TEXT_SIZE 160
+#define CONFIG_SCREEN_WIDTH 800
+#define CONFIG_SCREEN_HEIGHT 800
+#define CONFIG_TIMER_TEXT_SIZE 160
+#define CONFIG_TEXT_COLOR BLACK
+#define CONFIG_WORK_BACKGROUND_COLOR SKYBLUE
+#define CONFIG_REST_BACKGROUND_COLOR GREEN
+#define CONFIG_WORK_MINUTES 25
+#define CONFIG_REST_MINUTES 5
+#define CONFIG_START_RESTING true
 
 int main(int argc, char **argv)
 {
     (void)argc;
     (void)argv;
 
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Pomodoro Timer");
+    InitWindow(CONFIG_SCREEN_WIDTH, CONFIG_SCREEN_HEIGHT, "Pomodoro Timer");
     SetTargetFPS(60);
 
-    bool is_resting = false;
-    ssize_t minute_count = 0;
-    ssize_t seconds_count = 3;
+    bool is_resting = CONFIG_START_RESTING;
+    ssize_t minute_count = CONFIG_WORK_MINUTES;
+    ssize_t seconds_count = 0;
+    if (is_resting == true) {
+        minute_count = CONFIG_REST_MINUTES;
+    }
 
     float elapsed_time = 0;
     while (!WindowShouldClose()) {
@@ -50,20 +59,23 @@ int main(int argc, char **argv)
 
         BeginDrawing();
         if (is_resting == true) {
-            ClearBackground(GREEN);
+            ClearBackground(CONFIG_REST_BACKGROUND_COLOR);
         } else {
-            ClearBackground(SKYBLUE);
+            ClearBackground(CONFIG_WORK_BACKGROUND_COLOR);
         }
 
         char line_buffer[64] = {0};
         snprintf(line_buffer, 63, "%zu:%zu", minute_count, seconds_count);
 
-        int text_size = MeasureText(line_buffer, TIMER_TEXT_SIZE);
+        int text_size = MeasureText(line_buffer, CONFIG_TIMER_TEXT_SIZE);
         Vector2 text_position = (Vector2) {
-            (SCREEN_WIDTH - text_size) / 2.0f,
-            (SCREEN_HEIGHT - TIMER_TEXT_SIZE) / 2.0f
+            (CONFIG_SCREEN_WIDTH - text_size) / 2.0f,
+            (CONFIG_SCREEN_HEIGHT - CONFIG_TIMER_TEXT_SIZE) / 2.0f
         };
-        DrawText(line_buffer, text_position.x, text_position.y, TIMER_TEXT_SIZE, BLACK);
+        DrawText(line_buffer,
+                 text_position.x, text_position.y,
+                 CONFIG_TIMER_TEXT_SIZE,
+                 CONFIG_TEXT_COLOR);
 
         EndDrawing();
     }
